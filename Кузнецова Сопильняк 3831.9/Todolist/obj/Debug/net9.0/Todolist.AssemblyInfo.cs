a5d7bb54282a5c48ec4a05847edde24aa85cd6af
YadrenBaton <grifon423@gmail.com>
2025-09-26 21:28:08 +0300

    комит

--- Кузнецова Сопильняк 3831.9/Todolist/obj/Debug/net9.0/Todolist.AssemblyInfo.cs
+++ Кузнецова Сопильняк 3831.9/Todolist/obj/Debug/net9.0/Todolist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+946dfdfa3d585d7555d1a267f92be5e864b90ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fe7b9fd79ebe32440d8480238a32c93d3bbb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
